bc4582d95538d1207f6e641cc8481b25cb5dd117
DimonInc <example@mail.com>
2025-11-01 22:12:27 +0300

    The main is ready, pages index and about

--- main/views.py
+++ main/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request) -> HttpResponse:
+    context: dict[str, str] = {
+        'title': 'Home',
+        'content': 'Магазин мебели HOME'
+    }
+    return render(request, 'main/index.html', context)
+
+def about(request) -> HttpResponse:
+    context: dict[str, str] = {
+        'title': 'О нас',
+        'content': 'О нас тоже',
+        'text_on_page': 'Неплохо, хороший товар'
+    }
+    return render(request, 'main/about.html', context)
