8dfa354100b3a5c9239501e1c593d8dd1a7eb326
DimonInc <example@mail.com>
2025-11-08 03:47:35 +0300

    Registred simple tag for categories

--- app/settings.py
+++ app/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
+    "debug_toolbar",
+    
     'main',
     'goods',
     
@@ -51,6 +53,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'app.urls'
@@ -121,6 +125,18 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS: list[Path] = [BASE_DIR / 'static',]
+
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
+
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
