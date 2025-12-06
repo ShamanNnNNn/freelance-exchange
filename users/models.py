3bf1a9f02950f278f6a1214ee7861b97906fe224
DimonInc <example@mail.com>
2025-12-06 18:06:01 +0300

    Login form ready

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+class User(AbstractUser):
+    image = models.ImageField(upload_to='users_images', blank=True, null=True)
+    
+    class Meta:
+        db_table = 'user'
+        
+    def __str__(self):
+        return self.username
