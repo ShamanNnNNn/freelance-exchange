9d97a02b586f7c638c72a3f7a75d2a641cd30863
DimonInc <example@mail.com>
2026-06-30 23:41:17 +0300

    fix psycopg2 and remove contrib.postgres

--- app/settings.py
+++ app/settings.py
@@ -28,7 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.postgres',
+    #'django.contrib.postgres',
     'main',
     'users',
     'order',
